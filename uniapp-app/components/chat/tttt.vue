de4ae6d1cefb0f08d17ee2e398574e9bbdbc1174
mjy <296629801@qq.com>
2021-01-12 18:16:39 +0800

    1

---
+++ uniapp-app/components/chat/tttt.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<!-- @功能 -->
+		<!-- <view class="process" v-show="_call_s.length>0" @click="processFunc">
+			<text>当前{{_call_s.length}}人@我</text>
+		</view> -->
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
