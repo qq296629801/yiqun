22b3ce8a6caec6d448ad014c181aa0d0c145e447
mjy <296629801@qq.com>
2021-03-07 16:34:54 +0800

    1

---
+++ vue-app/unpackage/cache/wgt/__UNI__54ED11E/static/emoji/ChatUtils.js
@@ -0,0 +1,282 @@
+// 格式化时间
+export function formatDateTime(date) {
+	let y = date.getFullYear();
+	let m = date.getMonth() + 1;
+	m = m < 10 ? '0' + m : m;
+	let d = date.getDate();
+	d = d < 10 ? '0' + d : d;
+	let h = date.getHours();
+	h = h < 10 ? '0' + h : h;
+	let minute = date.getMinutes();
+	minute = minute < 10 ? '0' + minute : minute;
+	let second = date.getSeconds();
+	second = second < 10 ? '0' + second : second;
+	return y + '-' + m + '-' + d + ' ' + h + ':' + minute + ':' + second;
+}
+
+/**
+ * 毫秒转换友好的显示格式
+ * 输出格式：21小时前
+ * @param  {[type]} date 日期
+ */
+export function dateStr(date) {
+	// 获取js 时间戳
+	let time = new Date().getTime();
+	// 去掉 js 时间戳后三位
+	time = parseInt((time - date * 1000) / 1000);
+	// 存储转换值
+	let s;
+	if (time < 60 * 10) {
+		// 十分钟内
+		return '刚刚';
+	} else if (time < 60 * 60 && time >= 60 * 10) {
+		// 超过十分钟少于1小时
+		s = Math.floor(time / 60);
+		return s + '分钟前';
+	} else if (time < 60 * 60 * 24 && time >= 60 * 60) {
+		// 超过1小时少于24小时
+		s = Math.floor(time / 60 / 60);
+		return s + '小时前';
+	} else if (time < 60 * 60 * 24 * 3 && time >= 60 * 60 * 24) {
+		// 超过1天少于3天内
+		s = Math.floor(time / 60 / 60 / 24);
+		return s + '天前';
+	} else {
+		// 超过3天
+		var date = new Date(value); //时间戳为10位需*1000，时间戳为13位的话不需乘1000
+		var Y = date.getFullYear() + '-';
+		var M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
+		var D = date.getDate() + ' ';
+		var h = date.getHours() + ':';
+		var m = date.getMinutes() + ':';
+		var ss = date.getSeconds();
+		return h + m + ss;
+	}
+}
+
+/**
+ * 聊天会话
+ */
+export class Chat {
+	constructor(id, name, avatar, unReadCount, lastMessage, mobile, email, type) {
+		this.id = id;
+		this.name = name;
+		this.avatar = avatar;
+		this.unReadCount = unReadCount;
+		this.lastMessage = lastMessage;
+		this.mobile = mobile;
+		this.email = email;
+		this.type = type;
+	}
+}
+
+/**
+ * 图片加载完成处理函数
+ * @param arr 图片的src集合
+ * @returns {Promise}
+ */
+function preloadImages(arr) {
+	let loadedImage = 0;
+	let images = [];
+	return new Promise(function(resolve, reject) {
+		for (let i = 0; i < arr.length; i++) {
+			images[i] = new Image();
+			images[i].src = arr[i];
+			images[i].onload = function() {
+				loadedImage++;
+				if (loadedImage === arr.length) {
+					resolve();
+				}
+			};
+			images[i].onerror = function() {
+				reject();
+			};
+		}
+	});
+}
+
+/**
+ * 图片加载完成，聊天对话框scroll拉到最下
+ * @param id 容器id
+ */
+export function imageLoad(id) {
+	scrollBottom(id);
+	let messageBox = document.getElementById(id);
+	if (messageBox) {
+		let images = messageBox.getElementsByTagName('img');
+		if (images) {
+			let arr = [];
+			for (let i = 0; i < images.length; i++) {
+				arr[i] = images[i].src;
+			}
+			preloadImages(arr)
+				.then(() => {
+					scrollBottom(id);
+				})
+				.catch(function() {
+					scrollBottom(id);
+				});
+		}
+	}
+}
+
+/**
+ * 滚动条到最下方
+ * @param id 容器id
+ */
+function scrollBottom(id) {
+	let div = document.getElementById(id);
+	if (div) {
+		div.scrollTop = div.scrollHeight;
+	}
+}
+const list = [
+  "微笑", "撇嘴", "色", "发呆", "得意", "流泪", "害羞", "闭嘴", "睡", "大哭", "尴尬", "发怒", "调皮", "呲牙", "惊讶", "难过", "囧", "抓狂", "吐", "偷笑", "愉快", "白眼", "傲慢", "困", "惊恐", "流汗", "憨笑", "悠闲", "奋斗", "咒骂", "疑问", "嘘", "晕", "衰", "骷髅", "敲打", "再见", "擦汗", "抠鼻", "鼓掌", "坏笑", "左哼哼", "右哼哼", "哈欠", "鄙视", "委屈", "快哭了", "阴险", "亲亲", "可怜", "菜刀", "西瓜", "啤酒", "咖啡", "猪头", "玫瑰", "凋谢", "嘴唇", "爱心", "心碎", "蛋糕", "炸弹", "便便", "月亮", "太阳", "拥抱", "强", "弱", "握手", "胜利", "抱拳", "勾引", "拳头", "OK", "跳跳", "发抖", "怄火", "转圈", "高兴", "口罩", "笑哭", "吐舌头", "傻呆", "恐惧", "悲伤", "不屑", "嘿哈", "捂脸", "奸笑", "机智", "皱眉", "耶", "鬼脸", "合十", "加油", "庆祝", "礼物", "红包", "鸡"
+]
+export let faceUtils = {
+	alt: list.map(item => `[${item}]`),
+	faces: () => {
+		let obj = {}
+    for (let i = 0; i < list.length; i++) {
+      const key = `[${list[i]}]`
+      obj[key] = `static/img/face/${i}.png`
+    }
+    return obj;
+	}
+};
+
+export function transform(content) {
+	// 支持的html标签
+	let html = function(end) {
+		return new RegExp('\\n*\\[' + (end || '') +
+			'(code|pre|div|span|p|table|thead|th|tbody|tr|td|ul|li|ol|li|dl|dt|dd|h2|h3|h4|h5)([\\s\\S]*?)]\\n*', 'g');
+	};
+	let fa = faceUtils.faces();
+	if (content) {
+		content = content
+			.replace(/&(?!#?[a-zA-Z0-9]+;)/g, '&amp;')
+			.replace(/</g, '&lt;')
+			.replace(/>/g, '&gt;')
+			.replace(/'/g, '&#39;')
+			.replace(/"/g, '&quot;') // XSS
+			.replace(/@(\S+)(\s+?|$)/g, '@<a href="javascript:;">$1</a>$2')
+
+			.replace(/\[([^\s\[\]]+?)]/g, function(face) {
+				// 转义表情
+				let alt = face.replace(/^/g, '');
+				return '<img width="30" alt="' + fa[alt] + '" title="' + fa[alt] + '" src="' + fa[alt] + '">';
+			})
+			.replace(/img\[([^\s]+?)]/g, function(img) {
+				// 转义图片
+				let href = img.replace(/(^img\[)|(]$)/g, '');
+				return '<img class="message-img" src="' + href + '" alt="消息图片不能加载">';
+			})
+			.replace(/file\([\s\S]+?\)\[[\s\S]*?]/g, function(str) {
+				// 转义文件
+				let href = (str.match(/file\(([\s\S]+?)\)\[/) || [])[1];
+				let text = (str.match(/\)\[([\s\S]*?)]/) || [])[1];
+				if (!href) return str;
+				return '<a class="message-file" href="' + href + '"><i class="ivu-icon ivu-icon-md-arrow-down"></i>' + (text ||
+					href) + '</a>';
+			})
+			.replace(/audio\[([^\s]+?)]/g, function(audio) {
+				// 转义音频
+				return '<div class="message-audio" data-src="' + audio.replace(/(^audio\[)|(]$)/g, '') +
+					'"><i class="layui-icon">&#xe652;</i><p>音频消息</p></div>';
+			})
+			.replace(/video\[([^\s]+?)]/g, function(video) {
+				// 转义音频
+				return '<div class="message-video"  data-src="' + video.replace(/(^video\[)|(]$)/g, '') +
+					'"><i class="layui-icon">&#xe652;</i></div>';
+			})
+			.replace(/a\([\s\S]+?\)\[[\s\S]*?]/g, function(str) {
+				// 转义链接
+				let href = (str.match(/a\(([\s\S]+?)\)\[/) || [])[1];
+				let text = (str.match(/\)\[([\s\S]*?)]/) || [])[1];
+				if (!href) return str;
+				return '<a href="' + href + '" target="_blank">' + (text || href) + '</a>';
+			})
+			.replace(html(), '<$1 $2>')
+			.replace(html('/'), '</$1>') // 转移HTML代码
+			.replace(/\n/g, '<br>'); // 转义换行
+	}
+	return content;
+}
+
+export const ChatListUtils = {
+	listKey: '_chatList',
+	setChatList: function(userId, chatList) {
+		localStorage.setItem(userId + this.listKey, JSON.stringify(chatList));
+	},
+	//从缓存中获取已经保存的会话
+	getChatList: function(userId) {
+		let str = localStorage.getItem(userId + this.listKey);
+		if (!str) {
+			return [];
+		}
+		return JSON.parse(str);
+	},
+	//删除聊天会话框
+	delChat: function(userId, chat) {
+		let tempChatList = [];
+		for (let item of this.getChatList(userId)) {
+			if (String(item.id) !== String(chat.id)) {
+				tempChatList.push(item);
+			}
+		}
+		// 放入缓存
+		this.setChatList(userId, tempChatList);
+		return tempChatList;
+	},
+	/**
+	 * 刷新会话列表
+	 * @param self 当前对象
+	 * @param user 用户
+	 * @param host 主机名
+	 * @param type 0 单聊 1 群聊
+	 * @returns {Chat} 当前会话
+	 */
+	resetChatList: function(self, user, host, type) {
+		console.log('user:' + JSON.stringify(user));
+		let chatList = this.getChatList(self.$store.state.user.id);
+		// 删除当前用户已经有的会话
+		let newChatList = chatList.filter(function(element) {
+			return String(element.id) !== String(user.id);
+		});
+
+		let avatar = user.avatar;
+		if (user.avatar.indexOf(host) === 0) {
+			avatar = user.avatar;
+		} else {
+			avatar = host + user.avatar;
+		}
+		// 重新添加会话，放到第一个
+		let chat = new Chat(user.id, user.name, avatar, 0, '', user.mobile, user.email, type);
+		newChatList.unshift(chat);
+		// 存储到localStorage 的 chatList
+		this.setChatList(self.$store.state.user.id, chatList);
+		self.$store.commit('setChatList', newChatList);
+		return chat;
+	}
+};
+//信息的类型 MSG_PING 心跳 、MSG_READY 链接就绪  MSG_MESSAGE 消息
+export const MessageInfoType = {
+	MSG_PING: '0',
+	MSG_READY: '1',
+	MSG_MESSAGE: '2'
+};
+//信息的目标类型 FRIEND 私聊 、CHAT_GROUP 群聊
+export const MessageTargetType = {
+	FRIEND: '0',
+	CHAT_GROUP: '1'
+};
+
+
+export const ErrorType = {
+	TIMEOUT_ERROR: 9, //超时
+	TOKEN_ERROR: 401, //token 失效错误
+	PARAM_ERROR: 400, //参数错误
+	FLUSH_TOKEN_ERROR: 7, //刷新token错误
+	SERVER_ERROR: 500, //服务器错误
+	NET_ERROR: 'TypeError: Failed to fetch' //网络链接不通
+};
---
+++ vue-app/unpackage/cache/wgt/__UNI__54ED11E/static/emoji/emojiData.js
@@ -0,0 +1,192 @@
+//模拟数据
+var  emojiData = {
+			"imgArr" : [
+				{
+				emojiName:"",
+				emojiSort:0,
+				minEmoji:false,
+				emojiPath:"",
+				emojiList: [
+					  [
+						{ url: '', alt: '[]' },﻿﻿{ url: '', alt: '[]' },﻿﻿{ url: '', alt: '[]' },﻿﻿{ url: '', alt: '[]' },
+					    { url: '', alt: '[]' },﻿﻿{ url: '', alt: '[]' },﻿﻿{ url: '', alt: '[]' },﻿﻿{ url: '', alt: '[]' },
+					 ],
+					 [
+						{ url: '', alt: '[]' },﻿﻿{ url: '', alt: '[]' },﻿﻿{ url: '', alt: '[]' },﻿﻿{ url: '', alt: '[]' },
+					    { url: '', alt: '[]' },﻿﻿{ url: '', alt: '[]' },﻿﻿{ url: '', alt: '[]' },﻿﻿{ url: '', alt: '[]' },
+					 ],
+					 [
+						{ url: '', alt: '[]' },﻿﻿{ url: '', alt: '[]' },﻿﻿{ url: '', alt: '[]' },﻿﻿{ url: '', alt: '[]' },
+					    { url: '', alt: '[]' },﻿﻿{ url: '', alt: '[]' },﻿﻿{ url: '', alt: '[]' },﻿﻿{ url: '', alt: '[]' },
+					 ]
+					]
+				},
+				{
+					// QQ 表情
+					emojiName:"QQemoji",  
+					emojiSort:1,
+					minEmoji:true,
+					emojiPath: "/static/img/face/",
+					emojiList: [
+							[
+								{ url: '1.png', alt: '[微笑]' },
+								{ url: '2.png', alt: '[撇嘴]' },
+								{ url: '3.png', alt: '[色]' },
+								{ url: '4.png', alt: '[发呆]' },
+								{ url: '5.png', alt: '[得意]' },
+								{ url: '6.png', alt: '[流泪]' },
+								{ url: '7.png', alt: '[害羞]' },
+								{ url: '8.png', alt: '[闭嘴]' },
+								{ url: '9.png', alt: '[睡]' },
+								{ url: '10.png', alt: '[大哭]' },
+								{ url: '11.png', alt: '[尴尬]' },
+								{ url: '12.png', alt: '[发怒]' },
+								{ url: '13.png', alt: '[调皮]' },
+								{ url: '14.png', alt: '[呲牙]' },
+								{ url: '15.png', alt: '[惊讶]' },
+								{ url: '16.png', alt: '[难过]' },
+								{ url: '17.png', alt: '[酷]' },
+								{ url: '18.png', alt: '[冷汗]' },
+								{ url: '19.png', alt: '[抓狂]' },
+								{ url: '20.png', alt: '[吐]' },
+								{ url: 'static/img/tab/delete2.png', alt: '[删除]' }
+							],
+							[
+								{ url: '21.png', alt: '[偷笑]' },
+								{ url: '22.png', alt: '[愉快]' },
+								{ url: '23.png', alt: '[白眼]' },
+								{ url: '24.png', alt: '[傲慢]' },
+								{ url: '25.png', alt: '[饥饿]' },
+								{ url: '26.png', alt: '[困]' },
+								{ url: '27.png', alt: '[惊恐]' },
+								{ url: '28.png', alt: '[流汗]' },
+								{ url: '29.png', alt: '[憨笑]' },
+								{ url: '30.png', alt: '[悠闲]' },
+								{ url: '31.png', alt: '[奋斗]' },
+								{ url: '32.png', alt: '[咒骂]' },
+								{ url: '33.png', alt: '[疑问]' },
+								{ url: '34.png', alt: '[嘘]' },
+								{ url: '35.png', alt: '[晕]' },
+								{ url: '36.png', alt: '[疯了]' },
+								{ url: '37.png', alt: '[衰]' },
+								{ url: '38.png', alt: '[骷髅]' },
+								{ url: '39.png', alt: '[敲打]' },
+								{ url: '40.png', alt: '[再见]' },
+								
+								{ url: 'static/img/tab/delete2.png', alt: '[删除]' }
+							],
+							[
+								{ url: '41.png', alt: '[擦汗]' },
+								{ url: '42.png', alt: '[抠鼻]' },
+								{ url: '43.png', alt: '[鼓掌]' },
+								{ url: '44.png', alt: '[糗大了]' },
+								{ url: '45.png', alt: '[坏笔]' },
+								{ url: '46.png', alt: '[左哼哼]' },
+								{ url: '47.png', alt: '[右哼哼]' },
+								{ url: '48.png', alt: '[哈欠]' },
+								{ url: '49.png', alt: '[鄙视]' },
+								{ url: '50.png', alt: '[委屈]' },
+								{ url: '51.png', alt: '[快哭了]' },
+								{ url: '52.png', alt: '[阴险]' },
+								{ url: '53.png', alt: '[亲亲]' },
+								{ url: '54.png', alt: '[吓]' },
+								{ url: '55.png', alt: '[可怜]' },
+								{ url: '56.png', alt: '[菜刀]' },
+								{ url: '57.png', alt: '[西瓜]' },
+								{ url: '58.png', alt: '[啤酒]' },
+								{ url: '59.png', alt: '[篮球]' },
+								{ url: '60.png', alt: '[乒乓]' },
+								
+								{ url: 'static/img/tab/delete2.png', alt: '[删除]' }
+							],
+							[
+								{ url: '61.png', alt: '[咖啡]' },
+								{ url: '62.png', alt: '[饭]' },
+								{ url: '63.png', alt: '[猪头]' },
+								{ url: '64.png', alt: '[玫瑰]' },
+								{ url: '65.png', alt: '[凋谢]' },
+								{ url: '66.png', alt: '[嘴唇]' },
+								{ url: '67.png', alt: '[爱心]' },
+								{ url: '68.png', alt: '[心碎]' },
+								{ url: '69.png', alt: '[蛋糕]' },
+								{ url: '70.png', alt: '[闪电]' },
+								{ url: '71.png', alt: '[炸弹]' },
+								{ url: '72.png', alt: '[刀]' },
+								{ url: '73.png', alt: '[足球]' },
+								{ url: '74.png', alt: '[瓢虫]' },
+								{ url: '75.png', alt: '[便便]' },
+								{ url: '76.png', alt: '[月亮]' },
+								{ url: '77.png', alt: '[太阳]' },
+								{ url: '78.png', alt: '[礼物]' },
+								{ url: '79.png', alt: '[拥抱]' },
+								{ url: '80.png', alt: '[强]' },	
+								
+								{ url: 'static/img/tab/delete2.png', alt: '[删除]' }
+							],
+							[
+								{ url: '81.png', alt: '[弱]' },
+								{ url: '82.png', alt: '[握手]' },
+								{ url: '83.png', alt: '[胜利]' },
+								{ url: '84.png', alt: '[抱拳]' },
+								{ url: '85.png', alt: '[勾引]' },
+								{ url: '86.png', alt: '[拳头]' },
+								{ url: '87.png', alt: '[差劲]' },
+								{ url: '88.png', alt: '[爱你]' },
+								{ url: '89.png', alt: '[NO]' },
+								{ url: '90.png', alt: '[OK]' },
+								{ url: '91.png', alt: '[爱情]' },
+								{ url: '92.png', alt: '[飞吻]' },
+								{ url: '93.png', alt: '[跳跳]' },
+								{ url: '94.png', alt: '[发抖]' },
+								{ url: '95.png', alt: '[怄火]' },
+								{ url: '96.png', alt: '[转圈]' },
+								{ url: '97.png', alt: '[磕头]' },
+								{ url: '98.png', alt: '[回头]' },
+								{ url: '99.png', alt: '[跳绳]' },
+								{ url: '100.png', alt: '[投降]' },
+								
+								{ url: 'static/img/tab/delete2.png', alt: '[删除]' }
+							],
+							[
+								{ url: '101.png', alt: '[激动]' },
+								{ url: '102.png', alt: '[乱舞]' },
+								{ url: '103.png', alt: '[献吻]' },
+								{ url: '104.png', alt: '[左太极]' },
+								{ url: '105.png', alt: '[右太极]' },
+								{ url: 'static/img/tab/delete2.png', alt: '[删除]' }
+							]
+						]
+				    },
+						{
+						emojiName:"",
+						emojiSort:8,
+						minEmoji:false,
+						emojiPath:"",
+						emojiList: [
+							  [
+								{ url: '', alt: '[]' },﻿﻿{ url: '', alt: '[]' },﻿﻿{ url: '', alt: '[]' },﻿﻿{ url: '', alt: '[]' },
+								{ url: '', alt: '[]' },﻿﻿{ url: '', alt: '[]' },﻿﻿{ url: '', alt: '[]' },﻿﻿{ url: '', alt: '[]' },
+							   ],
+							   [
+								{ url: '', alt: '[]' },﻿﻿{ url: '', alt: '[]' },﻿﻿{ url: '', alt: '[]' },﻿﻿{ url: '', alt: '[]' },
+								{ url: '', alt: '[]' },﻿﻿{ url: '', alt: '[]' },﻿﻿{ url: '', alt: '[]' },﻿﻿{ url: '', alt: '[]' },
+							   ],
+							   [
+								{ url: '', alt: '[]' },﻿﻿{ url: '', alt: '[]' },﻿﻿{ url: '', alt: '[]' },﻿﻿{ url: '', alt: '[]' },
+								{ url: '', alt: '[]' },﻿﻿{ url: '', alt: '[]' },﻿﻿{ url: '', alt: '[]' },﻿﻿{ url: '', alt: '[]' },
+							   ]
+							  ]
+						} 
+			     ]
+	    }
+
+
+
+
+// export与export default均可用于导出常量、函数、文件、模块等
+// 在一个文件或模块中，export、import可以有多个，export default仅有一个
+// 通过export方式导出，在导入时要加{ }，export default则不需要
+// export能直接导出变量表达式，export default不行。
+
+
+export default emojiData;
