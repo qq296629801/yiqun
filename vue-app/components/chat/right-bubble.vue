14afdd4da713a4e28f23c034ed91f4e723be5fa8
mjy <296629801@qq.com>
2021-03-17 15:36:13 +0800

    1

--- vue-app/components/chat/right-bubble.vue
+++ vue-app/components/chat/right-bubble.vue
@@ -19,7 +19,7 @@
 				</view>
 				<!-- 图片消息 -->
 				<view @longtap="openRight(row)" v-if="row.msgType==1" class="bubble img" @tap="showPic(`${$url}/${row.msgContext}`)">
-					<image :src="`${$url}/scale_${row.msgContext}`" style="width:100px;height:100px"></image>
+					<image :src="$url+'/scale_'+ row.msgContext" style="width:100px;height:100px"></image>
 				</view>
 				<!-- 语言消息 -->
 				<view @longtap="openRight(row)" v-if="row.msgType==3" class="bubble voice" @tap="playVoice(row)" :class="playMsgId == row.id?'play':''">
